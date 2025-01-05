ea25d298805462af2f4501051e4463d5768decab
ahmaddzakwan <wawangithub@gmail.com>
2025-01-05 20:32:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "selector",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      sans: ["Plus Jakarta Sans", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
